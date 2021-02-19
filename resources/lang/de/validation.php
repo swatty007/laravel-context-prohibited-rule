02af8db9f8f3c70b070fb8ecb85ff306e079041c
Martin Niehoff <info@martin-niehoff.de>
2021-02-19 23:23:51 +0100

    - Added additional translations for German & Dutch
    Signed-off-by: Martin Niehoff <info@martin-niehoff.de>

---
+++ resources/lang/de/validation.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Context Prohibited Validation Rule
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'context_prohibited' => 'Ihr :attribute darf keines der folgenden Wörter enthalten: :prohibited',
+];
---
+++ resources/lang/nl/validation.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Context Prohibited Validation Rule
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'context_prohibited' => 'Uw :attribute mag geen van de volgende woorden bevatten: :prohibited',
+];
